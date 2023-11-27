48562c176144be6852f0e3b792e2bbe34a0b7c25
KGS-Ben <benb@kgsresearch.com>
2023-11-27 18:14:29 -0500

    Export PassportDecorator for library

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import { AxiosDecorator } from './lib/axios-utils';
+import { PassportDecorator } from './lib/passport/PassportDecorator';
 
 module.exports = {
     AxiosDecorator,
+    PassportDecorator
 };
