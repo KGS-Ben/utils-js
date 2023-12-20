1af0bfb1c6d45223dd67ac82c62c29f410fb56e9
KGS-Ben <benb@kgsresearch.com>
2023-12-20 13:51:58 -0500

    Add LRU as export

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -4,9 +4,11 @@ export * from './lib/express-middlewares/validateRequest';
 import { AxiosDecorator } from './lib/axios-utils';
 import { PassportDecorator } from './lib/passport';
 import { validateBody, validateQuery } from './lib/express-middlewares/validateRequest';
+import { Lru } from './lib/lru';
 declare const _default: {
     AxiosDecorator: typeof AxiosDecorator;
     PassportDecorator: typeof PassportDecorator;
+    Lru: typeof Lru;
     expressMiddlewares: {
         validateBody: typeof validateBody;
         validateQuery: typeof validateQuery;
